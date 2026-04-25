034d6fc56dd75052c6b7a90990a8f44c80fb7049
rkats20449-arch <rkats20449@gmail.com>
2026-04-25 16:46:27 +0000

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-```jsx
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
@@ -8,4 +8,4 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
   </React.StrictMode>
 )
-```
+
